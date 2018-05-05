3bf22aa0d2bf0ddfeee8ff66a4846a4338803299
Igor Sakhankov <suharik333@mail.ru>
2018-05-05 12:23:30 +0200

    Add temp routing for different roles

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,22 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'DIAMCO' });
+    res.render('index', { title: 'DIAMCO' });
+});
+
+/* GET SPV page. */
+router.get('/spv', function(req, res, next) {
+    res.render('index', { title: 'DIAMCO' });
+});
+
+/* GET Investor page. */
+router.get('/investor', function(req, res, next) {
+    res.render('index', { title: 'DIAMCO' });
+});
+
+/* GET AWDC page. */
+router.get('/awdc', function(req, res, next) {
+    res.render('index', { title: 'DIAMCO' });
 });
 
 module.exports = router;
